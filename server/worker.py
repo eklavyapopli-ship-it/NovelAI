91fc4a4661b51644ea1473d127cd541cf8b841ff
eklavyapopli-ship-it <eklavyapopli@gmail.com>
2026-01-14 21:01:44 +0530

    memory layer done

--- server/worker.py
+++ server/worker.py
@@ -1,51 +1,66 @@
 from google import genai
 from pydantic import BaseModel, Field
 from openai import OpenAI
+from langchain_core.messages import SystemMessage, HumanMessage
 from typing import Annotated
 from typing_extensions import TypedDict
 from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
+from langchain.chat_models import init_chat_model
 from typing import Union
 import json
 import os
 from dotenv import load_dotenv
 from langchain.agents import create_agent
-
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.checkpoint.mongodb import MongoDBSaver
 load_dotenv()
 class State(TypedDict):
     messages: Annotated[list, add_messages]
-
-client = OpenAI(
-    api_key= os.getenv('GOOGLE_API_KEY'),
-    base_url= "https://generativelanguage.googleapis.com/v1beta/openai/"
+llm  = ChatGoogleGenerativeAI(
+    model="gemini-2.0-flash",
+    
 )
-
 
 systemPrompt = '''
 You are Libris, an insightful literary companion AI. You discuss novels, themes, and characters with depth, offering thoughtful interpretations, emotional insights, and reflective questions. Never quote books directly; focus on analysis, opinions, and engaging conversation. Adapt your tone to be cozy, dark, or philosophical depending on the user’s mood.
 Always respond in valid JSON format: {"response": "your reply here"}.
 '''
 
+def chatbot(state:State):
+    response = llm.invoke(state.get("messages"))
+    return {"messages":[response]}
+graph_builder = StateGraph(State)
+graph_builder.add_node("chatbot",chatbot)
+graph_builder.add_edge(START,"chatbot")
+graph_builder.add_edge("chatbot", END)
+
+graph = graph_builder.compile()
+
+def compile_graph_with_checkpointer(checkpointer):
+    return graph_builder.compile(checkpointer=checkpointer)
 
 
-def process(query: str):
-    message = [ {"role":"system", "content": systemPrompt} ,{"role":"user","content":query} ]
+
+
+def process(query:str):
     
-    
-    response = client.chat.completions.create(
-        model="gemini-2.0-flash",
-        response_format={
-        "type": "json_object"
-    },
-        temperature=0,
-        messages=message
-        
-    )
-    raw_content = response.choices[0].message.content
-    parsed_response = json.loads(raw_content)
-    return parsed_response["response"]
-
-
-
+    initial_state = {
+    "messages": [
+        SystemMessage(content=systemPrompt),
+        HumanMessage(content=query)
+    ]
+}
+    DB_URI = "mongodb://admin:admin@localhost:27017"
+    with MongoDBSaver.from_conn_string(DB_URI) as checkpointer:
+        graph_with_checkpointer = compile_graph_with_checkpointer(checkpointer=checkpointer)
+        config = {
+        "configurable":{
+            "thread_id":"khush"
+        }
+    }    
+        for chunk in graph_with_checkpointer.stream(State(initial_state),config, stream_mode="values"):
+            last_msg = chunk["messages"][-1]
+            if last_msg.type == "ai":
+                return last_msg.content
