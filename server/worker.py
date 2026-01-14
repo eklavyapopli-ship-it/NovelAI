74748c73c59f9fb1c12f7bed2e156e62e163db71
eklavyapopli-ship-it <eklavyapopli@gmail.com>
2026-01-14 16:34:26 +0530

    mongodb setup

--- server/worker.py
+++ server/worker.py
@@ -1,6 +1,10 @@
 from google import genai
 from pydantic import BaseModel, Field
 from openai import OpenAI
+from typing import Annotated
+from typing_extensions import TypedDict
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
 from typing import Union
 import json
 import os
@@ -9,15 +13,9 @@ from langchain.agents import create_agent
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langgraph.checkpoint.memory import InMemorySaver
 load_dotenv()
-class Novel(BaseModel):
-    BookName: str = Field(description="Name of the Novel")
-    AIopinion: str = Field(description="AI opinion / reply based on the user query strictly in accordance to novel")
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
 
-class Reply(BaseModel):
-    AIopinion: str = Field(description="AI opinion / reply based on the user query strictly in accordance to novel")
-class ModerationResult(BaseModel):
-    decision: Union[Novel, Reply]
-memory = []
 client = OpenAI(
     api_key= os.getenv('GOOGLE_API_KEY'),
     base_url= "https://generativelanguage.googleapis.com/v1beta/openai/"
@@ -33,7 +31,7 @@ Always respond in valid JSON format: {"response": "your reply here"}.
 
 def process(query: str):
     message = [ {"role":"system", "content": systemPrompt} ,{"role":"user","content":query} ]
-
+    
     
     response = client.chat.completions.create(
         model="gemini-2.0-flash",
@@ -47,5 +45,6 @@ def process(query: str):
     raw_content = response.choices[0].message.content
     parsed_response = json.loads(raw_content)
     return parsed_response["response"]
-    
+
+
 
